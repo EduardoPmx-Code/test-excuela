86475ec07a3a4c6e29f2de6808660920887993a0
Eduardopmx <eduardop.29.91@gmail.com>
2024-06-28 03:59:29 -0400

    add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {  HttpClientModule  } from "@angular/common/http";
+import { AtomicChartComponent } from './shared/components/atomic-chart/atomic-chart.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,7 +12,7 @@ import {  HttpClientModule  } from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
