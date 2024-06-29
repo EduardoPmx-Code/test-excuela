2089d0855c5e3e144517e7286d625d375d383539
Eduardopmx <eduardop.29.91@gmail.com>
2024-06-29 11:48:47 -0400

    set config chat module

--- src/app/modules/chat/chat-routing.module.ts
+++ src/app/modules/chat/chat-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { AutoLoginGuard } from 'src/app/core/guards/auto-login.guard';
 
 const routes: Routes = [
   {
     path:'',component:ChatComponent,
+    children:[
+      {
+        path:'',
+        redirectTo:'auth'
+      },
+      {
+        path:'auth',
+        loadChildren: () => import('../auth/auth.module')
+          .then((m) => m.AuthModule
+        ),
+        canLoad: [AutoLoginGuard]
+      },
+      {
+        path:'rooms',
+        loadChildren: () => import('../room/room.module')
+          .then((m) => m.RoomModule
+        ),
+        canLoad: [AuthGuard]
+      },
+      {
+        path: '**', 
+        redirectTo: 'auth',
+      },
+    ]
   }
 ];
 
