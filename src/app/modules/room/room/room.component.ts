835a65d5454f4097f4304a26ba6597813f46cb40
Eduardopmx <eduardop.29.91@gmail.com>
2024-07-01 10:15:03 -0400

    call dinamic room

--- src/app/modules/room/room/room.component.ts
+++ src/app/modules/room/room/room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
@@ -12,13 +13,22 @@ import { ChatRoom } from 'src/utils/interfaces';
 })
 export class RoomComponent implements OnInit {
   chatRooms$: Observable<ChatRoom[]>;
-  newRoomName: string = '';
+  createNewRoom!:FormGroup //newRoomName: string = '';
   private subscriptions: Subscription = new Subscription();
+  dinamicRoomId='';
+  loader = false;
+  showChat = false;
+  roomId: any;
 
   constructor(private authService:AuthService,
               private router:Router,
-              private chatRoomService:ChatRoomService
+              private chatRoomService:ChatRoomService,
+              private fb:FormBuilder
   ) {
+
+    this.createNewRoom =this.fb.group({
+      newRoomName: ['', [Validators.required]],
+    })
     this.chatRooms$ = this.chatRoomService.chatRooms$;
    }
 
@@ -28,10 +38,13 @@ export class RoomComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loader = true;
     this.chatRoomService.loadAllChatRooms();
     this.chatRooms$.subscribe(data=>{
       console.log(data)
+      this.loader = false
     })
+    //this.loader = false
   }
 
   ngOnDestroy(): void {
@@ -39,14 +52,24 @@ export class RoomComponent implements OnInit {
   }
 
   async createRoom() {
-    if (this.newRoomName.trim()) {
-      const roomId = await this.chatRoomService.createChatRoom(this.newRoomName);
-      this.router.navigate(['main','chat','rooms','single-room', roomId]);
-    }
+   let roomName =  this.createNewRoom.value.newRoomName
+
+      const roomId = await this.chatRoomService.createChatRoom(roomName);
+      //this.router.navigate(['main','chat','rooms','single-room', roomId]);
+    
   }
 
   joinRoom(roomId: string) {
     this.router.navigate(['main','chat','rooms','single-room', roomId]);
+  }
+
+  setDinamicRoom(id: string) {
+    console.log(id);
+    this.dinamicRoomId = id;
+    this. showChat = true
+  }
+  closeChat(){
+    this.showChat = false;
   }
 
 }
