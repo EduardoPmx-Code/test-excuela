38e8fe41c6d6046353a4cdb6fac99d9ad2a4aafb
Eduardopmx <eduardop.29.91@gmail.com>
2024-07-01 10:11:14 -0400

    add reactive forms

--- src/app/modules/auth/components/login/login/login.component.ts
+++ src/app/modules/auth/components/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {  Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 
@@ -8,22 +9,44 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email: string = '';
-  password: string = '';
-
+  //email: string = '';
+  //password: string = '';
+  credentials!:FormGroup
   constructor(private authService: AuthService,
-              private router: Router
-  ) {}
+              private router: Router,
+              private fb:FormBuilder
+  ) {
+    this.credentials = this.fb.group({
+      email:['', [Validators.required,Validators.email]],
+      password:['', [Validators.required]],
+    })
+  }
 
   login() {
-    this.authService.login(this.email, this.password)
+    /*this.authService.login(this.email, this.password)
       .then(res => {
         console.log('Login successful!', res);
         this.router.navigate(['main','chat','rooms'])
       })
       .catch(err => {
         console.error('Login error', err);
-      });
+      });*/
+      let email = this.credentials.value.email
+      let password = this.credentials.value.password
+      console.log( email, password)
+      if(this.credentials.status === "VALID"){
+        this.authService.login(email, password)
+        .then(res => {
+          console.log('Login successful!', res);
+          this.router.navigate(['main','chat','rooms'])
+        })
+        .catch(err => {
+          console.error('Login error', err);
+        });
+      }else{
+        alert("invalid ")
+      }
+     //
   }
 
   ngOnInit(): void {
