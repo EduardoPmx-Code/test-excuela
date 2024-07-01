d728144c23a2cbe8cef7f62404abe19ccb15fbad
Eduardopmx <eduardop.29.91@gmail.com>
2024-07-01 10:13:43 -0400

    add video interface

--- src/utils/interfaces/index.ts
+++ src/utils/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './interface.chats';
 export * from './chat.room.interface';
 export * from './message.interface';
+export * from './video.interface';
---
+++ src/utils/interfaces/video.interface.ts
@@ -0,0 +1,11 @@
+export interface SourceVideo{
+    src:string;
+    type:string;
+    label:string;
+  }
+  export interface trackSubtitle{
+    src: string;
+    kind: 'subtitles';
+    srclang: string;
+    label: string;
+  }
