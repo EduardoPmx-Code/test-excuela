d1c25b0dd86532d193b9cffb96834f7297be4d20
Eduardopmx <eduardop.29.91@gmail.com>
2024-07-01 10:11:47 -0400

    add reactive forms

--- src/app/modules/auth/components/register/register/register.component.ts
+++ src/app/modules/auth/components/register/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 
@@ -8,19 +9,31 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  email: string = '';
-  password: string = '';
-  username: string = '';
+  credentrials!: FormGroup
+  //email: string = '';
+  //password: string = '';
+  //username: string = '';
 
   constructor(private authService: AuthService,
-              private router:Router
-  ) {}
+              private router:Router,
+              private fb:FormBuilder
+  ) {
+    this.credentrials = fb.group({
+      username:['', [Validators.required]],
+      email:['', [Validators.required,Validators.email]],
+      password:['', [Validators.required]],
+    })
+  }
   ngOnInit(): void {
     
   }
 
   register() {
-    this.authService.register(this.email, this.password, this.username)
+    if(this.credentrials.status === "VALID"){
+      let email = this.credentrials.value.email;
+      let password = this.credentrials.value.password
+      let username = this.credentrials.value.username
+      this.authService.register(email, password, username)
       .then(res => {
         console.log('Registration successful!', res);
         this.router.navigate(['main','chat','login'])
@@ -28,6 +41,10 @@ export class RegisterComponent implements OnInit {
       .catch(err => {
         console.error('Registration error', err);
       });
+    }else{
+      alert("invalid ")
+    }
+   
   }
 
 }
