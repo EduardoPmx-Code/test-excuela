8b90f922f6ee2e55ed949ad73cede8827eba19b6
Eduardopmx <eduardop.29.91@gmail.com>
2024-07-01 10:09:32 -0400

    set config shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtomicChartComponent } from './components/atomic-chart/atomic-chart.component';
+import { AtomicVideoComponent } from './components/atomic-video/atomic-video.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { AtomicChatRoomComponent } from './components/atomic-chat-room/atomic-chat-room.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [AtomicChartComponent],
+  declarations: [AtomicChartComponent,
+    AtomicVideoComponent,
+    LoaderComponent,
+    AtomicChatRoomComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports:[
-    AtomicChartComponent
+    AtomicChartComponent,
+    AtomicVideoComponent,
+    LoaderComponent,
+    AtomicChatRoomComponent
   ]
 })
 export class SharedModule { }
